c77beedf7effa1d0fa17184decf2d59d260763f7
Jalome <chjalome@gmail.com>
2022-10-04 11:55:14 +0200

    added more animated views

--- App.js
+++ App.js
@@ -39,7 +39,7 @@ const App: () => Node = () => {
         rotateAnim,
         {
           toValue: 45,
-          duration: 1200,
+          duration: 2000,
           easing: Easing.linear,
           useNativeDriver: true,
         }
@@ -50,39 +50,99 @@ const App: () => Node = () => {
 
   return (
     <SafeAreaView style={styles.mainView}>
-      <Animated.View style={[styles.animatedView,
-          {
-            transform:
-            [
-              // {translateX:rotateAnim},
-              {
-                rotate:rotateAnim.interpolate({
-                  inputRange:[0,45],
-                  outputRange:['0deg','360deg']
-                })
-              }
-            ],
-            borderRadius:rotateAnim.interpolate({
-              inputRange:[0,25,45],
-              outputRange:[100,100,50]
-            }),
-            backgroundColor:'#009688',        
-          }
-        ]}>
-
-          <Animated.Text
-            style={{
-              opacity:rotateAnim.interpolate({
-                inputRange:[0,44,45],
-                outputRange:[0,0,1]
+        <Animated.View style={[styles.animatedMainView,
+            {
+              transform:
+              [
+                // {translateX:rotateAnim},
+                {
+                  rotate:rotateAnim.interpolate({
+                    inputRange:[0,45],
+                    outputRange:['0deg','-360deg']
+                  })
+                }
+              ],
+              borderRadius:rotateAnim.interpolate({
+                inputRange:[0,25,45],
+                outputRange:[100,100,80]
               }),
-              color:'white',
-            }}
-          >
-            {/* Rotation is complete */}
-          </Animated.Text>
+            }
+          ]}>
 
-      </Animated.View>
+
+            {/* <Animated.Text
+              style={{
+                opacity:rotateAnim.interpolate({
+                  inputRange:[0,44,45],
+                  outputRange:[0,0,1]
+                }),
+                color:'white',
+              }}
+            >
+        
+            </Animated.Text> */}
+
+        </Animated.View>
+
+        <Animated.View style={[styles.animatedSecondView,
+            {
+              transform:
+              [
+                // {translateX:rotateAnim},
+                {
+                  rotate:rotateAnim.interpolate({
+                    inputRange:[0,45],
+                    outputRange:['0deg','360deg']
+                  })
+                }
+              ],
+              borderRadius:rotateAnim.interpolate({
+                inputRange:[0,25,45],
+                outputRange:[100,100,70]
+              }),
+            }
+        ]}>
+        </Animated.View>
+
+        <Animated.View style={[styles.animatedThirdView,
+            {
+              transform:
+              [
+                // {translateX:rotateAnim},
+                {
+                  rotate:rotateAnim.interpolate({
+                    inputRange:[0,45],
+                    outputRange:['0deg','-360deg']
+                  })
+                }
+              ],
+              borderRadius:rotateAnim.interpolate({
+                inputRange:[0,25,45],
+                outputRange:[100,100,70]
+              }),
+            }
+        ]}>
+        </Animated.View>
+
+        <Animated.View style={[styles.animatedFourthView,
+            {
+              transform:
+              [
+                // {translateX:rotateAnim},
+                {
+                  rotate:rotateAnim.interpolate({
+                    inputRange:[0,45],
+                    outputRange:['0deg','360deg']
+                  })
+                }
+              ],
+              borderRadius:rotateAnim.interpolate({
+                inputRange:[0,25,45],
+                outputRange:[100,100,20]
+              }),
+            }
+        ]}>
+        </Animated.View>
     </SafeAreaView>
   );
 };
@@ -94,12 +154,41 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems:'center'
   },
-  animatedView:{
+  animatedMainView:{
     width:300,
     height:300,
     justifyContent:'center',
     alignItems:'center',
-    borderRadius:12,
+    borderRadius:1,
+    backgroundColor:'#009688',   
+    position:'absolute',
+
+  },
+  animatedSecondView:{
+    width:280,
+    height:280,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:1,
+    backgroundColor:'#ff5722',
+    position:'absolute',
+  },
+  animatedThirdView:{
+    width:260,
+    height:260,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:1,
+    backgroundColor:'#e91e63',
+    position:'absolute',
+  },
+  animatedFourthView:{
+    width:220,
+    height:220,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:1,
+    backgroundColor:'#673ab7',
   }
 });
 
